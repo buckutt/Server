438db44e0602b3ed42d9f77185358c16f028122f
Gabriel JUCHAULT <gjuchault@users.noreply.github.com>
2016-04-18 22:50:48 +0200

    Handle rethink errors (#34)
    * Exit application if database server is not running
    
    * Switch back to thinky's rethinkdbdash

--- src/lib/thinky.js
+++ src/lib/thinky.js
@@ -1,4 +1,18 @@
 import thinky from 'thinky';
 import config from '../config';
+import logger from './log';
 
-export default thinky({ db: config.db });
+const log = logger(module);
+
+const conn = thinky({ db: config.db, silent: true });
+
+/* istanbul ignore next */
+conn.r
+    .getPoolMaster()
+    .getConnection()
+    .catch(() => {
+        log.error('Connection to DB failed.');
+        process.exit(1);
+    });
+
+export default conn;
