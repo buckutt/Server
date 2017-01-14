fde04aafe3da217b863863af364e255f9ccfec53
Gabriel JUCHAULT <gjuchault@users.noreply.github.com>
2017-01-14 14:37:06 +0100

    feat(db): retry db connections before exiting (#137)
    * feat(db): retry db connections before exiting

--- src/lib/thinky.js
+++ src/lib/thinky.js
@@ -1,15 +1,23 @@
 const thinky = require('thinky');
 const config = require('../../config');
 const logger = require('./log');
+const retry  = require('./retry');
 
 const log = logger(module);
 
 const conn = thinky({ host: config.db.host, db: config.db.name, silent: true });
 
+const allowUnhandled = () => {};
+process.on('unhandledRejection', allowUnhandled);
+
+const pool = conn.r.getPoolMaster();
+
 /* istanbul ignore next */
-conn.r
-    .getPoolMaster()
-    .getConnection()
+retry(() => pool.getConnection(), 10, 3000, () => log.debug('Retrying DB connection...'))
+    .then(() => {
+        log.info('Connected to DB.');
+        process.removeListener('unhandledRejection', allowUnhandled);
+    })
     .catch(() => {
         log.error('Connection to DB failed.');
         process.exit(1);
