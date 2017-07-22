8068c83bf68b018c67139f286a50c7ebd60c102d
Lucas Soulier <rinke2005@gmail.com>
2017-07-22 16:28:30 +0200

    fix: rights checking at login (#225)

--- src/lib/canSellOrReload.js
+++ src/lib/canSellOrReload.js
@@ -1,6 +1,7 @@
 const config = require('../../config');
 
-module.exports = (user, connectType = 'pin') => {
+module.exports = (user, connectType = 'pin', pointId) => {
+    const now    = new Date();
     const result = { canSell: false, canReload: false };
 
     /* istanbul ignore if */
@@ -9,7 +10,9 @@ module.exports = (user, connectType = 'pin') => {
     }
 
     for (const right of user.rights) {
-        if (!right.isRemoved && !right.period.isRemoved) {
+        if (!right.isRemoved && !right.period.isRemoved &&
+            right.period.start <= now && right.period.end > now &&
+            right.Point_id === pointId) {
             const configRight = config.rights[right.name];
 
             if (right.name === 'admin' && connectType !== 'pin') {
