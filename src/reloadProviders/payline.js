69cf4e451c204519cbc14d8e4cfdc51d51e59fbb
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-01-23 18:35:28 +0100

    fix: add buyer/seller

--- src/reloadProviders/payline.js
+++ src/reloadProviders/payline.js
@@ -124,11 +124,12 @@ module.exports = (app) => {
                         credit  : transaction.get('amount'),
                         type    : 'card-online',
                         trace   : transaction.get('id'),
-                        point_id: req.point_id
-                    })
-                    .save();
+                        point_id: req.point_id,
+                        buyer_id : transaction.get('user_id'),
+                        seller_id: transaction.get('user_id')
+                    });
 
-                    return Promise.all([ userCredit, newReload, transaction.save() ]);
+                    return Promise.all([ userCredit, newReload.save(), transaction.save() ]);
                 }
 
                 return transaction.save();
