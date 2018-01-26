dc066ef835f06ae3a60b2cc91f52d4ba2b6c178e
Lucas Soulier <rinke2005@gmail.com>
2018-01-26 14:29:08 +0100

    fix(manager): generate bcrypt on server side (#271)

--- src/controllers/services/manager/changepin.js
+++ src/controllers/services/manager/changepin.js
@@ -18,22 +18,23 @@ router.put('/services/manager/changepin', (req, res, next) => {
 
     const models = req.app.locals.models;
 
-    if (req.body.currentPin.length !== 4) {
-        next(new APIError(module, 401, 'Current PIN has to be clear, not crypted'));
+    if (req.body.currentPin.length !== 4 && req.body.pin.length !== 4) {
+        next(new APIError(module, 401, 'PINs has to be clear, not crypted'));
     }
 
-    bcrypt.compareAsync(req.body.currentPin.toString(), req.user.pin)
-        .then(match =>
-            new Promise((resolve, reject) => {
-                if (match) {
-                    return resolve();
-                }
+    let cryptedPin;
 
-                reject(new APIError(module, 401, 'PIN is wrong'));
-            }))
-        .then(() => models.User.where({ id: req.user.id }).fetch())
+    bcrypt.compareAsync(req.body.currentPin.toString(), req.user.pin)
+        .then(match => (match
+            ? Promise.resolve()
+            : Promise.reject(new APIError(module, 401, 'PIN is wrong'))))
+        .then(() => bcrypt.hash(req.body.pin, 10))
+        .then((hash) => {
+            cryptedPin = hash;
+            return models.User.where({ id: req.user.id }).fetch();
+        })
         .then((user) => {
-            user.set('pin', req.body.pin);
+            user.set('pin', cryptedPin);
             user.set('updated_at', new Date());
 
             return user.save();
